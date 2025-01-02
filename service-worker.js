5ac4bc538ea99c84bbd77b2d2ca863cbfbbd3c8a
masatt <43009366+masatt@users.noreply.github.com>
2025-01-02 11:03:41 +0900

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+// キャッシュ名とキャッシュ対象のファイルを定義
+const CACHE_NAME = 'app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/beat-generator.html',
+  '/other-app.html'
+];
+
+// インストールイベント
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// フェッチイベントでキャッシュを提供
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// アクティベーションイベントで古いキャッシュを削除
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
